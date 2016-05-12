112254c603e89d19f8efb7d399ceeaab36ceb3c2
rburinsky <UB02U20@ubalt.edu>
2016-05-12 14:29:41 -0400

    Find AD Object based on Modified Date
    Example powershell script to find objects in AD that were modified based
    on the date used in variable.

---
+++ AD_findmodifydate.ps1
@@ -0,0 +1,12 @@
+﻿# One year ago
+$d = [DateTime]::Today.AddDays(-360)
+
+# Show desktops only that have not been modified in 1 year
+$olddesktops = get-adcomputer -filter 'Modified -le $d' -properties Modified | Where-Object {$_.name -like '*dt*'} | FT Name, Modified | format-table name 
+
+# Show anything that has not been modified in 1 year
+$oldeverything = get-adcomputer -filter 'Modified -le $d' -properties Modified | FT Name, Modified | format-table name
+
+ # Deleted old desktops that have not been modified in a year
+ # Remove process will begin as soon as you highlight and run command
+$deleteold = get-adcomputer -filter 'Modified -le $d' -properties Modified | Where-Object {$_.name -like '*dt*'} | remove-adcomputer
