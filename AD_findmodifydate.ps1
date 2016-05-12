dcb0bd4276a6496fe087e5a496793ef2e31c3338
rburinsky <UB02U20@ubalt.edu>
2016-05-12 15:05:52 -0400

    Added Comment:  Need local AD tools

--- AD_findmodifydate.ps1
+++ AD_findmodifydate.ps1
@@ -1,8 +1,9 @@
-﻿# One year ago
+# AD client tools need to be installed on local system.
+# One year ago
 $d = [DateTime]::Today.AddDays(-360)
 
 # Show desktops only that have not been modified in 1 year
-$olddesktops = get-adcomputer -filter 'Modified -le $d' -properties Modified | Where-Object {$_.name -like '*dt*'} | FT Name, Modified | format-table name 
+$olddesktops = get-adcomputer -filter 'Modified -le $d' -properties Modified | Where-Object {$_.name -like '*dt*'} | FT Name, Modified | format-table name
 
 # Show anything that has not been modified in 1 year
 $oldeverything = get-adcomputer -filter 'Modified -le $d' -properties Modified | FT Name, Modified | format-table name
