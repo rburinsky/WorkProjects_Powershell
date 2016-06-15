74f65a2fa9d5bffa80fca2cad31bac50f20538f8
rburinsky <UB02U20@ubalt.edu>
2016-06-15 15:35:22 -0400

    Custom Star Menu Folder with Office 2016 Shortcuts
    Some users on Windows 7 still want Office application shortcuts grouped
    in one start menu folder.  Office 2016 is meant for Windows 10 and the
    "tiled format", therefore when installing on Windows 7, the shortcuts
    are scattered on the main program bar.
    
    This script simply created a new folder, loops through all of the
    shortcuts and copies the shortcuts with the wild card express *2016* in
    the name to the newly created programs folder.

---
+++ Office2016_StartMenu.ps1
@@ -0,0 +1,41 @@
+﻿#Save all Office 2016 shortcuts to a new folder on the Start Menu specifically for Windows 7 users
+#$ErrorActionPreference= 'silentlycontinue'
+Clear-Host
+
+
+$Path = "$Env:ProgramData\Microsoft\Windows\Start` Menu\Programs"
+# Create new folder in Start Menu called Office 2016
+new-item "$Env:ProgramData\Microsoft\Windows\Start` Menu\Programs\Office` 2016" -type directory -force
+$newdest = "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Office` 2016"
+
+# Get all shortcuts in Start Menu
+$StartMenu = Get-ChildItem $Path -Recurse -Include *.lnk
+
+# Wildcard match for Office 2016
+$match = "*2016*"
+
+# Loop through each shortcut looking for $match (wildcard)
+ForEach ($Item in $StartMenu) {
+
+
+
+if ($newdiritem = $Item | where-object {$_.Name -like $match}){
+
+#Keep quote in file path
+$newdiritemupdate = '"{0}"' -f $newdiritem
+
+#copy all shortcuts that match to new folder
+copy-item $newdiritem -destination $newdest -Force
+
+#write-host to confirm paths
+write-host $newdiritemupdate
+
+#Reset variable for next loop intineration
+$newdiritemupdate = ""
+            }
+
+#No action for non Office 2016 files
+else{
+$newdiritem = ""
+}
+}
